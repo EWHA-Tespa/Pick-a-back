586e67d4af00ef5811efce7bdd25734a664540e2
JisooJeong <113654733+JisooJudy@users.noreply.github.com>
2024-11-13 20:59:32 +0900

    Update baseline.sh

--- run_pickaback/baseline.sh
+++ run_pickaback/baseline.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 DATASETS=(
     'None'                # dummy
     'aquatic_mammals'
@@ -33,15 +32,19 @@ seed=2
 ####################
 ##### Baseline #####
 ####################
-TASK_ID=4
 
-CUDA_VISIBLE_DEVICES=$GPU_ID python3 packnet_cifar100_main_normal.py \
-    --arch $ARCH \
-    --dataset ${DATASETS[TASK_ID]} --num_classes 5 \
-    --lr 1e-2 \
-    --weight_decay 4e-5 \
-    --save_folder checkpoints_${ARCH}/baseline_scratch/$ARCH/${DATASETS[TASK_ID]} \
-    --epochs $FINETUNE_EPOCHS \
-    --mode finetune \
-    --logfile logs_${ARCH}/baseline_cifar100_acc_scratch.txt \
-    --seed $seed        
+for TASK_ID in {1..20}
+do
+    CUDA_VISIBLE_DEVICES=$GPU_ID python3 packnet_cifar100_main_normal.py \
+        --arch $ARCH \
+        --dataset ${DATASETS[TASK_ID]} --num_classes 5 \
+        --lr 1e-2 \
+        --weight_decay 4e-5 \
+        --save_folder checkpoints_${ARCH}/baseline_scratch/$ARCH/${DATASETS[TASK_ID]} \
+        --epochs $FINETUNE_EPOCHS \
+        --mode finetune \
+        --logfile logs_${ARCH}/baseline_cifar100_acc_scratch.txt \
+        --seed $seed
+done
+# 실행하고 log_lenet5/baseline_cifar100_acc_scratch.txt 확인
+# 자세하게 log 확인해보고 싶다면 open_checkpoint.py 실행
