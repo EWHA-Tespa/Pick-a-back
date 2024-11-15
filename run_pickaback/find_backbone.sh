37d7aecffc7a6c58c029dca87355a8d673c4310b
mummy-alive <mummyee@ewhain.net>
2024-11-15 00:52:51 -0500

    Add column name on

--- run_pickaback/find_backbone.sh
+++ run_pickaback/find_backbone.sh
@@ -1,5 +1,13 @@
 
 GPU_ID=0
+RESULT_FILE="find_backbone_result.csv"
+
+if [ -f $RESULT_FILE ]; then
+    rm $RESULT_FILE
+fi
+
+echo "target_id,selected_backbone" > $RESULT_FILE
+
 for TARGET_ID in {1..20}
 do
     CUDA_VISIBLE_DEVICES=$GPU_ID TARGET_ID=$TARGET_ID python3 pickaback_cifar100.py
